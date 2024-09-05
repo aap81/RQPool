4c7630b6582ad9cf5256a18a54d52503b1d7a385
AAP <alexaaron120@gmail.com>
2024-09-05 14:10:05 -0700

    optimzie

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--data', default='AIDS', help='Dataset used')
 parser.add_argument('--lr', type=float, default=1e-3, help='Learning rate')
 parser.add_argument('--batchsize', type=int, default=256, help='Training batch size')
-parser.add_argument('--nepoch', type=int, default=20, help='Number of training epochs')
+parser.add_argument('--nepoch', type=int, default=100, help='Number of training epochs')
 parser.add_argument('--hdim', type=int, default=128, help='Hidden feature dim')
 parser.add_argument('--width', type=int, default=4, help='Width of GCN')
 parser.add_argument('--depth', type=int, default=6, help='Depth of GCN')
@@ -20,13 +20,17 @@ parser.add_argument('--seed', type=int, default=10, help='Random seed')
 parser.add_argument('--patience', type=int, default=50, help='Patience')
 parser.add_argument('--intergraph', default='sage', help="mean or max or attention or none")
 parser.add_argument('--alltests', type=int, default=0, help='Run all tests for the data and hyperparameter')
-parser.add_argument('--datagroup', type=int, default=1, help="select dataset group")
+parser.add_argument('--datagroup', type=int, default=0, help="select dataset group")
 parser.add_argument('--outputfile', default='', help="set output file for logs")
 parser.add_argument('--completedindex', type=int, default=0, help="completed_index")
 parser.add_argument('--endindex', type=int, default=999999999, help="end_index")
 
 args = parser.parse_args()
 
+if args.outputfile == '':
+    args.outputfile = 'output.txt'
+else:
+    args.outputfile = f"output-{args.outputfile}.txt"
 
 if args.outputfile == '':
     args.outputfile = 'output.txt'
@@ -98,24 +102,33 @@ if args.alltests == 1:
         index += 1
 elif args.alltests == 2:
     datasets = DATASETS
+    intergraphs = []
+    if args.datagroup == 1:
+        intergraphs = ["none", "sage", "mean"]
+    else:
+        intergraphs = ["sort", "set2set", "max"]
+
     completed_index = args.completedindex
     end_index = args.endindex
     index = 1
-    total_tests = len(datasets)
-    for dataset in datasets:
-        if index > completed_index and index < end_index:
-            args.data = dataset
-            args.lr = 1e-3
-            args.batchsize = 256
-            args.hdim = 64
-            args.width = 4
-            args.depth = 6
-            args.dropout = 0.4
-            args.decay = 0  # Set decay value
-            log_print(f"Group by {dataset}, Test number: {index}/{total_tests}", args.outputfile)
-            test.execute(args)
-        else:
-            log_print(f"Test number: {index}/{total_tests} skipped", args.outputfile)
-        index += 1
+    total_tests = len(datasets) * len(intergraphs)
+    for intergraph in intergraphs:
+        args.outputfile = f"output-{intergraph}.txt"
+        for dataset in datasets:
+            if index > completed_index and index < end_index:
+                args.data = dataset
+                args.lr = 1e-3
+                args.batchsize = 256
+                args.hdim = 64
+                args.width = 4
+                args.depth = 6
+                args.intergraph = intergraph
+                args.dropout = 0.4
+                args.decay = 0  # Set decay value
+                log_print(f"Group by {dataset}, Test number: {index}/{total_tests}", args.outputfile)
+                test.execute(args)
+            else:
+                log_print(f"Test number: {index}/{total_tests} skipped", args.outputfile)
+            index += 1
 else:
     test.execute(args)
