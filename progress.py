f2813017e4db8c66e2798de1ad8644f38c97d0b9
AAP <alexaaron120@gmail.com>
2024-09-05 17:05:53 -0700

    added test auc line

--- progress.py
+++ progress.py
@@ -11,6 +11,7 @@ for intergraph in intes:
             content = file.read()
 
         groups = content.split("Group by ")[1:]
+        metrics = {}
         for group in groups:
             dataset = group.split(", Test number:")[0]
             epochs = group.split("Epoch: ")[1:]
@@ -19,11 +20,19 @@ for intergraph in intes:
                 completed[dataset] = last_epoch_index
             else:
                 completed[dataset] = 'not started yet'
+            if 'Test auc: ' in group:
+                metrics[dataset] = []
+                # pdb.set_trace()
+                for auc_val in group.split('Test auc: ')[1:]:
+                    metrics[dataset].append('Test auc: ' + auc_val.split("\n")[0])
 
     print(f"Intergraph: {intergraph}")
     if len(completed.keys()) == 0:
         print(f"     Not Started yet")    
     for dataset in completed.keys():
         print(f"     {dataset} -  Step: {completed[dataset]}")
+        if dataset in metrics.keys():
+            for auc_val in metrics[dataset]:
+                print(f"       Test line: {auc_val}")
     print("")
 
