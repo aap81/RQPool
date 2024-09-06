0d05823f543b797e70a81748da0fddffd7aa5145
AAP <alexaaron120@gmail.com>
2024-09-05 20:46:12 -0700

    improved the dict for progress

--- progress.py
+++ progress.py
@@ -3,36 +3,43 @@ from copy import deepcopy
 import os
 
 intes = ['none', 'sort', 'sage', 'set2set', 'mean', 'max']
-for intergraph in intes:
-    file_name = f"output-{intergraph}.txt"
-    completed = {}
+completed = {}
+metrics = {}
+
+for _intergraph in intes:
+    file_name = f"output-{_intergraph}.txt"
     if os.path.exists(file_name):
         with open(file_name, 'r') as file:
             content = file.read()
-
         groups = content.split("Group by ")[1:]
-        metrics = {}
         for group in groups:
+            intergraph = group.split('"intergraph": "')[1].split('"')[0]
             dataset = group.split(", Test number:")[0]
+            if intergraph not in completed.keys():
+                completed[intergraph] = {}
+                metrics[intergraph] = {}
+            if dataset not in completed[intergraph].keys():
+                completed[intergraph][dataset] = {}
+                metrics[intergraph][dataset] = []
             epochs = group.split("Epoch: ")[1:]
             last_epoch_index = len(epochs) - 1
             if last_epoch_index > 0:
-                completed[dataset] = last_epoch_index
+                completed[intergraph][dataset] = last_epoch_index
             else:
-                completed[dataset] = 'not started yet'
+                completed[intergraph][dataset] = 'not started yet'
             if 'Test auc: ' in group:
-                metrics[dataset] = []
                 # pdb.set_trace()
                 for auc_val in group.split('Test auc: ')[1:]:
-                    metrics[dataset].append('Test auc: ' + auc_val.split("\n")[0])
+                    metrics[intergraph][dataset].append('Test auc: ' + auc_val.split("\n")[0])
 
+for intergraph in intes:
     print(f"Intergraph: {intergraph}")
-    if len(completed.keys()) == 0:
-        print(f"     Not Started yet")    
-    for dataset in completed.keys():
-        print(f"     {dataset} -  Step: {completed[dataset]}")
-        if dataset in metrics.keys():
-            for auc_val in metrics[dataset]:
-                print(f"       Test line: {auc_val}")
+    if intergraph in completed.keys():
+        for dataset in completed[intergraph].keys():
+            print(f"     {dataset} -  Step: {completed[intergraph][dataset]}")
+            if len(metrics[intergraph][dataset]) > 0:
+               for auc_val in metrics[intergraph][dataset]:
+                print(f"       Test line: {auc_val}") 
+    else:
+        print(f"     Not Started yet")
     print("")
-
